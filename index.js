a2902a7d8c4641042b259ca5853637c4541797a6
CosBett <cosmasbett9@gmail.com>
2021-11-14 15:20:51 +0300

    Add day of the week formula function

--- index.js
+++ index.js
@@ -35,3 +35,16 @@ const year = document.querySelector("#year").innerHTML
     }
   }
   document.getElementById("month").addEventListener("change", validateMonth)
+
+   function akanName(){
+       let year = document.getElementById("year").value
+       let yr = String(year).split("")
+            .map((year)=>{
+                return Number(year)
+            })
+       let CC = yr.slice(0,2).join("")
+       let YY = yr.slice(2)
+       let MM= document.getElementById("month")
+       let DD =document.getElementById("day")
+       const d =( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+   }
